cd8487bf5566b0d8a4df7ac2fd5e0873431a04a2
Andres G <AndresG_99_@hotmail.com>
2025-01-17 15:15:39 -0500

    Integracion libreria de CSS y correcion del login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 
 Vue.use(VueRouter)
@@ -11,22 +10,8 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Login',
     component: LoginView
-  },
-
-  {
-    path: '/',
-    name: 'homeView',
-    component: HomeView
-  },
-
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
+
 ]
 
 const router = new VueRouter({
